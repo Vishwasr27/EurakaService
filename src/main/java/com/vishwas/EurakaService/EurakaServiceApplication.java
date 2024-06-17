3411d03ce928deedcce2ea7a82717fdd034fb5f2
unknown <vishwasr5159@gmail.com>
2024-06-17 13:38:19 +0530

    first commit euraka service

---
+++ src/main/java/com/vishwas/EurakaService/EurakaServiceApplication.java
@@ -0,0 +1,23 @@
+package com.vishwas.EurakaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurakaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishwas/EurakaService/EurakaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwas.EurakaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
